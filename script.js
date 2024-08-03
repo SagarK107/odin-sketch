2c4c8983bd9cdf1be3bd9c59fc611d89e8fcab62
SagarK107 <sagarkarambelkar107@gmail.com>
2024-08-03 09:23:19 +0530

    Added javascript functions to create grid on button press; Added border to sketchpad

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 
 const body = document.getElementById("container");
 
-for (var i = 0; i < 4; i++)
+function createGrid(gridSize)
 {
-    const row_div = document.createElement("div");
-    row_div.id = `row_${i}`
-    body.appendChild(row_div);
-    
-    for(var j = 0; j < 4; j++)
-    {
-        const column_div = document.createElement("div");
-        const grid_text = `value_${i}_${j}`;
-        const grid_text_node = document.createTextNode(grid_text);
-        column_div.id = `column_${j}`
-        column_div.appendChild(grid_text_node);
-        row_div.appendChild(column_div);
-
-
+    for (var i = 0; i < gridSize; i++)
+        {
+            const row_div = document.createElement("div");
+            row_div.id = `row_${i}`
+            body.appendChild(row_div);
+            
+            for(var j = 0; j < gridSize; j++)
+            {
+                const column_div = document.createElement("div");
+                // const grid_text = `value_${i}_${j}`;
+                // const grid_text_node = document.createTextNode(grid_text);
+                // column_div.appendChild(grid_text_node);
+                column_div.id = `column_${j}`
+                
+                row_div.appendChild(column_div);
         
+        
+                
+            }
+        }
+        
+}
+
+function popUpResize()
+{
+    const prompt = window.prompt("Enter new grid size (between 1 and 100)");
+    const gridSize = parseInt(prompt);
+    console.log(gridSize);
+    if (isNaN(gridSize))
+    {
+        window.alert("Invalid number, please input an integer");
     }
+    if (gridSize > 100 || gridSize < 1)
+    {
+        window.alert("Number should be between 1 and 100");
+    }
+    else{
+        
+        createGrid(gridSize);
+    }
+
 }
